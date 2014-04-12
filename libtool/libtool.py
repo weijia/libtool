0655a14c003f6e3dca098e730a6bf56a36dc064b
Richard <richardwangwang@gmail.com>
2014-04-12 16:38:27 +0800

    Give functions a more accurate name.

--- libtool/libtool.py
+++ libtool/libtool.py
@@ -72,6 +72,10 @@ def exclude(folder):
         sys.path.remove(folder)
 
 
-def get_parent_folder(file_path):
+def get_parent_folder_for_file(file_path):
     #print "parent:"+os.path.abspath(os.path.join(os.path.dirname(file_path),".."))
     return os.path.abspath(os.path.join(os.path.dirname(file_path),".."))
+
+
+def get_parent_folder_for_folder(folder_path):
+    return os.path.abspath(os.path.join(file_path,".."))
