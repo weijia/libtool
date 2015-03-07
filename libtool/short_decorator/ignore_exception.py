1ec61d9379639d2a465ab16378b86953e3e7132d
Richard <richardwangwang@gmail.com>
2015-03-07 12:33:29 +0800

    Added decorator to handle specific exception.

--- libtool/short_decorator/ignore_exception.py
+++ libtool/short_decorator/ignore_exception.py
@@ -2,11 +2,18 @@ import traceback
 
 
 def ignore_exc(func):
-    def wrap_with_exc():
-        #noinspection PyBroadException
-        try:
-            func()
-        except:
-            traceback.print_exc()
-            pass
-    return wrap_with_exc
+    return ignore_exc_with_result(func)
+
+
+def ignore_exc_with_result(exception_result=None, exception=Exception):
+    #Ref: http://wklken.me/posts/2012/10/27/python-base-decorator.html
+    def exc_wrapper(func):
+        def wrap_with_exc():
+            #noinspection PyBroadException
+            try:
+                return func()
+            except exception, e:
+                traceback.print_exc()
+                return exception_result
+        return wrap_with_exc
+    return exc_wrapper
