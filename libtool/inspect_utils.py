8068b1041733b7d706cfba256439547c6d6053ad
Richard <richardwangwang@gmail.com>
2015-06-09 10:17:13 +0800

    Added folder processors.

--- libtool/inspect_utils.py
+++ libtool/inspect_utils.py
@@ -12,3 +12,11 @@ def get_inspection_frame(frame_index):
 def get_parent_frame_file():
     frame_index = 2
     return get_inspection_frame(frame_index)
+
+
+def class_enumerator(module_instance, exclude_name_list=[]):
+    for name, obj in inspect.getmembers(module_instance):
+        if inspect.isclass(obj):
+            if name in exclude_name_list:
+                continue
+            yield obj
